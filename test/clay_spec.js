71c302d6a6166c250d693c60d1750e275da79816
shaw3257 <shaw3257@gmail.com>
2014-05-28 21:47:11 -0400

    adds testing

---
+++ test/clay_spec.js
@@ -0,0 +1,12 @@
+describe('Clay Layout', function(){
+
+	beforeEach(function (){
+		document.body.innerHTML = __html__['test/fixtures/index.html']
+	});
+
+	it('should work', function(){
+		expect(true).toBe(true);
+
+	});
+
+});
